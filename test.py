db92469ada5c7a7edf0f894a81e2a4c18481190b
anshv1811 <118555171+anshv1811@users.noreply.github.com>
2024-03-18 14:50:12 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,238 @@
+#1 Import Necessary Libraries:
+import warnings
+warnings.filterwarnings('ignore')
+
+import cv2
+import numpy as np
+import urllib.request
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPooling2D
+from keras.layers import Dropout, Flatten, BatchNormalization
+from keras.regularizers import l2
+from keras.optimizers import Adam
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from keras.models import load_model
+
+#2 Download CIFAR-10 dataset from keras library:
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+#3 Split original training data to training and validation sets:
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.1, random_state=0)
+
+#4 Printing out the dimensions of our training, validation, and test datasets:
+print('Train Images Shape:      ', X_train.shape)
+print('Train Labels Shape:      ', y_train.shape)
+
+print('\nValidation Images Shape: ', X_valid.shape)
+print('Validation Labels Shape: ', y_valid.shape)
+
+print('\nTest Images Shape:       ', X_test.shape)
+print('Test Labels Shape:       ', y_test.shape)
+
+#5  Let's take an overview of the CIFAR-10 dataset:
+
+
+# CIFAR-10 classes
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+               
+# Create a new figure
+plt.figure(figsize=(15,15))
+
+# Loop over the first 25 images
+for i in range(64):
+    # Create a subplot for each image
+    plt.subplot(8, 8, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+
+    # Display the image
+    plt.imshow(X_train[i])
+
+    # Set the label as the title
+    plt.title(class_names[y_train[i][0]], fontsize=12)
+
+# Display the figure
+plt.show()
+
+#6 Data Preprocessing
+
+#6.1 Normalization of Image Data
+'''
+convert the pixel values data type to float32 type, and then normalizes them by 
+subtracting the mean and dividing by the standard deviation of the training set, 
+enhancing the model's training efficiency and effectiveness:
+'''
+# Convert pixel values data type to float32
+X_train = X_train.astype('float32')
+X_test  = X_test.astype('float32')
+X_valid = X_valid.astype('float32')
+
+# Calculate the mean and standard deviation of the training images
+mean = np.mean(X_train)
+std  = np.std(X_train)
+
+# Normalize the data
+# The tiny value 1e-7 is added to prevent division by zero
+X_train = (X_train-mean)/(std+1e-7)
+X_test  = (X_test-mean) /(std+1e-7)
+X_valid = (X_valid-mean)/(std+1e-7)
+
+#6.2 One-Hot Encoding of Labels
+'''
+convert the class labels to one-hot vectors to transform the categorical labels into a
+format suitable for multi-class classification by neural networks:
+'''
+y_train = to_categorical(y_train, 10)
+y_valid = to_categorical(y_valid, 10)
+y_test  = to_categorical(y_test, 10)
+
+#6.3 Data Augmentation
+
+data_generator = ImageDataGenerator(
+    # Rotate images randomly by up to 15 degrees i.e. -15 to 15 degrees
+    rotation_range=15,
+    
+    # Shift images horizontally by up to 12% of their width
+    width_shift_range=0.12,
+    
+    # Shift images vertically by up to 12% of their height
+    height_shift_range=0.12,
+    
+    # Randomly flip images horizontally
+    horizontal_flip=True,
+    
+    # Zoom images in by up to 10%
+    zoom_range=0.1,
+    
+    # Change brightness by up to 10%
+    brightness_range=[0.9,1.1],
+
+    # Shear intensity (shear angle in counter-clockwise direction in degrees)
+    shear_range=10,
+    
+    # Channel shift intensity
+    channel_shift_range=0.1,
+)
+
+
+
+#7 Define CNN Model Architecture
+
+# Initialize a sequential model
+model = Sequential()
+
+# Set the weight decay value for L2 regularization
+weight_decay = 0.0001
+
+# Add the first convolutional layer with 32 filters of size 3x3
+model.add(Conv2D(filters=32, kernel_size=(3,3), padding='same', activation='relu', kernel_regularizer=l2(weight_decay), 
+                 input_shape=X_train.shape[1:]))
+# Add batch normalization layer
+model.add(BatchNormalization())
+
+# Add the second convolutional layer similar to the first
+model.add(Conv2D(filters=32, kernel_size=(3,3), padding='same', activation='relu', kernel_regularizer=l2(weight_decay)))
+model.add(BatchNormalization())
+
+# Add the first max pooling layer with pool size of 2x2
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# Add dropout layer with 0.2 dropout rate
+model.add(Dropout(rate=0.2))
+
+# Add the third and fourth convolutional layers with 64 filters
+model.add(Conv2D(filters=64, kernel_size=(3,3), padding='same', activation='relu', kernel_regularizer=l2(weight_decay)))   
+model.add(BatchNormalization())
+model.add(Conv2D(filters=64, kernel_size=(3,3), padding='same', activation='relu', kernel_regularizer=l2(weight_decay)))
+model.add(BatchNormalization())
+
+# Add the second max pooling layer and increase dropout rate to 0.3
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.3))
+
+# Add the fifth and sixth convolutional layers with 128 filters
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding='same', activation='relu', kernel_regularizer=l2(weight_decay))) 
+model.add(BatchNormalization())
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding='same', activation='relu', kernel_regularizer=l2(weight_decay)))
+model.add(BatchNormalization())
+
+# Add the third max pooling layer and increase dropout rate to 0.4
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.4))
+
+# Add the seventh and eighth convolutional layers with 256 filters
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding='same', activation='relu', kernel_regularizer=l2(weight_decay))) 
+model.add(BatchNormalization())
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding='same', activation='relu', kernel_regularizer=l2(weight_decay)))
+model.add(BatchNormalization())
+
+# Add the fourth max pooling layer and increase dropout rate to 0.5
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.5))
+
+# Flatten the tensor output from the previous layer
+model.add(Flatten())
+
+# Add a fully connected layer with softmax activation function for outputting class probabilities
+model.add(Dense(10, activation='softmax'))
+
+
+
+model.summary()
+
+#8 Training the CNN Model
+
+# Set the batch size for the training
+batch_size = 64
+
+# Set the maximum number of epochs for the training
+epochs = 300
+
+# Define the optimizer (Adam)
+optimizer = Adam(learning_rate=0.0005)
+
+# Compile the model with the defined optimizer, loss function, and metrics
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Add ReduceLROnPlateau callback
+# Here, the learning rate will be reduced by half (factor=0.5) if no improvement in validation loss is observed for 10 epochs
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, min_lr=0.00001)
+
+# Add EarlyStopping callback
+# Here, training will be stopped if no improvement in validation loss is observed for 40 epochs.
+# The `restore_best_weights` parameter ensures that the model weights are reset to the values from the epoch
+# with the best value of the monitored quantity (in this case, 'val_loss').
+early_stopping = EarlyStopping(monitor='val_loss', patience=40, restore_best_weights=True, verbose=1)
+
+# Fit the model on the training data, using the defined batch size and number of epochs
+# The validation data is used to evaluate the model's performance during training
+# The callbacks implemented are learning rate reduction when a plateau is reached in validation loss and
+# stopping training early if no improvement is observed
+model.fit(data_generator.flow(X_train, y_train, batch_size=batch_size),
+          epochs=epochs,
+          validation_data=(X_valid, y_valid),
+          callbacks=[reduce_lr, early_stopping], 
+          verbose=2)
+
+plt.figure(figsize=(15,6))
+
+# Plotting the training and validation loss
+plt.subplot(1, 2, 1)
+plt.plot(model.history.history['loss'], label='Train Loss', color='#8502d1')
+plt.plot(model.history.history['val_loss'], label='Validation Loss', color='darkorange')
+plt.legend()
+plt.title('Loss Evolution')
+
+# Plotting the training and validation accuracy
+plt.subplot(1, 2, 2)
+plt.plot(model.history.history['accuracy'], label='Train Accuracy', color='#8502d1')
+plt.plot(model.history.history['val_accuracy'], label='Validation Accuracy', color='darkorange')
+plt.legend()
+plt.title('Accuracy Evolution')
+
+plt.show()
